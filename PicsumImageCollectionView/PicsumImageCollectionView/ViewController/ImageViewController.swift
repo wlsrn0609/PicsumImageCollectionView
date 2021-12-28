97885306e3d857f4ed6333ea7206f120bbb7108a
KwonJinGu <wlsrn0609@m2community.co.kr>
2021-12-28 16:09:28 +0900

    주석 제거

--- PicsumImageCollectionView/PicsumImageCollectionView/ViewController/ImageViewController.swift
+++ PicsumImageCollectionView/PicsumImageCollectionView/ViewController/ImageViewController.swift
@@ -96,38 +96,8 @@ class ImageViewController : UIViewController {
             break
         }
 
-
-        
         return
         
-        
-//        let percentThreshold:CGFloat = 0.3
-//        let translation = gesture.translation(in: view)
-//        let verticalMovement = translation.y / view.bounds.height
-//        let downwardMovement = fmaxf(Float(verticalMovement), 0)
-//        let downwardMovementPercent = fminf(downwardMovement,1.0)
-//        let progress = CGFloat(downwardMovementPercent)
-//
-//        guard let interactor = interactor else {
-//            return
-//        }
-//
-//        switch gesture.state {
-//        case .began:
-//            interactor.hasStarted = true
-//            dismiss(animated: true, completion: nil)
-//        case .changed:
-//            interactor.shouldFinish = progress > percentThreshold
-//            interactor.update(progress)
-//        case .cancelled:
-//            interactor.hasStarted = false
-//            interactor.cancel()
-//        case .ended:
-//            interactor.hasStarted = false
-//            interactor.shouldFinish ? interactor.finish() : interactor.cancel()
-//        default:
-//            break
-//        }
     }
     
     @objc func close(){
@@ -161,5 +131,26 @@ class ImageViewController : UIViewController {
             alertCon.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
             self.present(alertCon, animated: true, completion: nil)
         }
+    }
+    
+    func showHelperCircle(){
+        let center = CGPoint(x: view.bounds.width * 0.5, y: 100)
+        let small = CGSize(width: 30, height: 30)
+        let circle = UIView(frame: CGRect(origin: center, size: small))
+        circle.layer.cornerRadius = circle.frame.width / 2
+        circle.backgroundColor = UIColor.white
+        circle.layer.shadowOpacity = 0.8
+        circle.layer.shadowOffset = CGSize.zero
+        self.view.addSubview(circle)
+        UIView.animate(withDuration: 0.5, delay: 0.25) {
+            circle.frame.origin.y += 200
+            circle.layer.opacity = 0
+        } completion: { _ in
+            circle.removeFromSuperview()
+        }
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showHelperCircle()
     }
 }
